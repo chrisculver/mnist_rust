a1db4395d7351409515d857b3bdd8a0b17c54c56
chrisculver <chris.cb.culver@gmail.com>
2021-11-25 17:00:41 +0000

    Working on containers for weights/biases

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Implementation of the python code at 
 http://neuralnetworksanddeeplearning.com/chap1.html
 but in Rust!
+
+The first version will just attempt to copy the structure there in as similar a manner as possible.
--- src/neural_net.rs
+++ src/neural_net.rs
@@ -1,21 +1,80 @@
+use std::ops;
+use std::ops::{Index, IndexMut};
+
 // the sigmoid function
 trait Sigmoid {
-    fn sigmoid(&self) -> f64;
+    fn sigmoid(self) -> f64;
+    fn sigmoid_prime(self) -> f64;
 }
 
 impl Sigmoid for f64 {
-    fn sigmoid(&self) -> f64 {
-        1.0/(1.0 + (-self).exp())
+    fn sigmoid(self) -> f64 {
+        1_f64/(1_f64 + (-self).exp())
+    }
+
+    fn sigmoid_prime(self) -> f64 {
+        self.sigmoid()*(1_f64-self.sigmoid())
     }
 }
 
+struct Vector { 
+    elems: Vec<f64>
+}
 
-// a neural network layer,
+impl Vector {
+    fn len(&self) -> usize {
+        self.elems.len()
+    }
+}
+
+impl Index<usize> for Vector 
+{
+    type Output = f64;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.elems[index]
+    }
+}
+
+impl IndexMut<usize> for Vector
+{
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        self.elems.index_mut(index)
+    }
+}
+
+impl ops::Add<Vector> for Vector {
+    type Output = Vector;
+    fn add(self, rhs: Vector) -> Vector {
+        let mut res = Vector{ elems: Vec::<f64>::new()};
+        for i in 0..self.len() {
+            res[i]=self[i]+rhs[i]
+        }
+
+        res
+    }
+}
+
+struct Matrix {
+    elems: Vec<Vec<f64>>
+}
+
+
 struct Network {
    num_layers: usize, //len of sizes
    sizes: Vec<usize>,
-   biases: Vec<
-   weights: 
+   weights: Vec<Matrix>,
+   biases: Vec<Vector>,
+}
+
+impl Network {
+    fn new(sizes: Vec<usize>) -> Self {
+        Network {
+            num_layers: sizes.len(),
+            sizes: sizes,
+            weights: Vec::<Matrix>::new(),
+            biases: Vec::<Vector>::new(),
+        }
+    }
 }
 
 
@@ -27,6 +86,8 @@ mod tests {
     #[test]
     fn some_sigmoid_values() {
         // e^0 = 1 so sigmoid(0.0) = 1/(1+1) = 0.5
-        assert_eq!(0.5,(0.0 as f64).sigmoid())
+        assert_eq!(0.5,(0_f64).sigmoid());
+
+        assert_eq!(0.25,(0_f64).sigmoid_prime());
     }
 }
